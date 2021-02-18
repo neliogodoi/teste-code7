5df79f7ea46ddd985faa1bd0225ad30619e03b9b
Nelio Godoi <nelioapp@gmail.com>
2021-02-17 22:27:15 -0300

    criado 'deploy-facil.sh'

--- deploy-facil.sh
+++ deploy-facil.sh
@@ -10,7 +10,6 @@ echo "=== Subindo API..."
 java -jar api-1.0.jar &
 cd ../Frontend/
 echo "=== Subindo Frontend..."
-cp debtmanage.zip /var/www/html/
-cd /var/www/html/
 unzip debtmanage.zip
+cp -r debtmanage/* /var/www/html/
 echo "=== Finzalizado!"
